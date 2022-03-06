273c612d33c116fa6bf831c55e14381c4009cda6
Reinhardt Zundorf <reinhardt@webfolders.co.za>
2022-03-06 18:34:29 +0200

    Debug file uploads.

--- src/app.py
+++ src/app.py
@@ -3,18 +3,18 @@
 from flask import Flask, request
 from pathlib import Path
 import requests
-from werkzeug import secure_filename
+
 
 app = Flask(__name__,
             static_url_path='/assets', 
             static_folder='assets',
             template_folder='templates')
 
-app.config['UPLOAD_FOLDER'] = 'uploads'
 
 @app.route("/", methods=["GET"])
 def form_example():
     return open('src/templates/index.html').read()
+
 
 @app.route("/", methods=["POST"])
 def extract_pdf_tables():
@@ -22,16 +22,15 @@ def extract_pdf_tables():
     file_type = request.form.get("file_type")
     dry_run = request.form.get("dry_run")
     
-    # Get the file.
-    file = request.files.get("file")
+    # Upload file.
+    uploaded_file = upload_file()
 
-    print(file)
-
-    return f'file_name: {file_name}<br>dry_run: {dry_run}<br>file_type: {file_type}<br>'
+    return f'upload_file_name{uploaded_file}'
+    # return f'file_name: {file_name}<br>dry_run: {dry_run}<br>file_type: {file_type}<br>'
 
 
 def upload_file():
    if request.method == 'POST':
-      f = request.files['file']
-      f.save(secure_filename(f.filename))
-      return 'file uploaded successfully'
+      f = request.files['file_pdf']
+      f.save(f'uploads/{f.filename}')
+      return f.filename
